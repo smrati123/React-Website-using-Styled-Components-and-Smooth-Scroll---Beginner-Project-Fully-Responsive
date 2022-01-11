3ed804e198c787d65ed06d0a2e4e1669d6f3dcfc
smrati123 <50202333+smrati123@users.noreply.github.com>
2022-01-11 18:54:37 +0530

    Add files via upload

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    darktext:false,
+    topline:'Premium Bank',
+    headline:'Unlimited Transaction with Zero Fess',
+    description:'Get acccess to uour App that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel:'Get Started',
+    imgStart:false,
+    img: require('../../images/undraw_off_road_9oae (1).png'),
+    alt:'Car',
+    dark:true,
+    primary:true,
+    darkText:false
+}
+
+export const homeObjTwo = {
+    id:'discover',
+    lightBg:true,
+    lightText:false,
+    darktext:false,
+    topline:'Unlimited Access',
+    headline:'Access Transaction with Zero Fess',
+    description:'Get acccess to uour App that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel:'Learn More',
+    imgStart:true,
+    img: require('../../images/istockphoto-1096969570-612x612.jpg'),
+    alt:'piggy bank',
+    dark:true,
+    primary:true,
+    darkText:true
+}
+
+export const homeObjThree = {
+    id:'signup',
+    lightBg:true,
+    lightText:false,
+    darktext:false,
+    topline:'jOin our Team',
+    headline:'Join our team with Zero Fess',
+    description:'Get acccess to our App that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel:'Start Now',
+    imgStart:false,
+    img: require('../../images/download.jpeg'),
+    alt:'Paper',
+    dark:true,
+    primary:true,
+    darkText:true
+}
