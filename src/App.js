267a9028b49939513c1730247efee73bdb6a0a48
smrati123 <50202333+smrati123@users.noreply.github.com>
2022-01-11 18:59:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Home from './pages';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import SigninPage from './pages/signin';
+// import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      {/* <Navbar/> */}
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/signIn" component={SigninPage} exact/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
